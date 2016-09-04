ca0ab76db46120fa276e28fe20ac8b892e1b2662
walker <917495744@qq.com>
2016-09-04 19:43:31 +0800

    实现响应式广告

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,13 @@
 /**
  * Created by Administrator on 2016/8/28.
  */
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items : 1,
+        loop:true,
+        autoplay:true,
+        autoplayTimeout:5000,
+
+        stagePadding:1
+    });
+});
